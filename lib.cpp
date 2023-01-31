dcd37948eac42ed09bbdf860e8b13d076bdb4ecb
haruto.baske.12@outlook.jp <haruto.baske.12@outlook.jp>
2023-01-31 10:48:57 +0900

    divideFiles

---
+++ lib.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+#include<iostream>
+void print(int i)
+{
+	std::cout << i;
+}
+
+void print(const char* s)
+{
+	std::cout << s;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include"lib.h"
 #include<iostream>
 int main() {
+	print("glu");
+	print("108");
 
 	system("pause>0");
 }
